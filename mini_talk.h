3e5c3fbdec138eae31e00018b6418a7efc820a1a
bigCoDult <qqqqmmss11@gmail.com>
2024-09-30 19:02:27 +0900

    add : signal.h sched.h

--- mini_talk.h
+++ mini_talk.h
@@ -1,8 +1,11 @@
 #ifndef MINI_TALK_H
 # define MINI_TALK_H
 
-# include "libft.h"
-
+// # include "libft.h"
+# include "./libft/libft.h"
+# include <unistd.h>
+# include <signal.h>
+# include <sched.h>
 
 
 #endif /* MINI_TALK_H */
