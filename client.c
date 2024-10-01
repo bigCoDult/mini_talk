c9e2a83652c4618b52f606c2a0cbe6c35d3ec246
bigCoDult <qqqqmmss11@gmail.com>
2024-10-01 17:12:12 +0900

    feat : send_string()

--- client.c
+++ client.c
@@ -1,32 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanbaek <sanbaek@student.42gyeongsan.kr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 17:11:01 by sanbaek           #+#    #+#             */
+/*   Updated: 2024/10/01 17:11:32 by sanbaek          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-/*
-You can have one global variable per program
-(one for the client and one for the server),
-but you will have to justify their use.
-
-signal()
-sigaction()
-
-sigemptyset()
-sigaddset()
-
-getpid()
-
-kill()
-
-pause
-sleep
-usleep
-eixt
-
-*/
 #include "mini_talk.h"
 
 int	main(int argc, char **argv)
 {
 	if (argc != 3)
 	{
-		ft_printf("%d",write(2, "Error\n: improper argc\n", 23));
+		write(2, "Error\n: improper argc\n", 23);
 		return (1);
 	}
 	if (validate_str(argv[2]))
@@ -35,7 +25,7 @@ int	main(int argc, char **argv)
 	return (0);
 }
 
-int validate_str(char *str)
+int	validate_str(char *str)
 {
 	int	i;
 
@@ -57,65 +47,27 @@ int validate_str(char *str)
 	return (1);
 }
 
-void	send_string(pid_t server_pid, char *str)
-{
-	int	i;
-	i = 0;
-	int *bit;
-	bit = ft_atob(str);
-	send_bit(server_pid, bit);
-	return ;
-}
-
-int	*ft_atob(char *str)
+char	*send_string(pid_t server_pid, char *str)
 {
 	int	i;
 	int	k;
-	int	*bit;
+	int	bit_up;
 
 	i = 0;
-	k = 0;
-	if (str == NULL)
-		return (NULL);
-	bit = (int *)ft_calloc(ft_strlen(str) * 8, sizeof(int));
-	if (bit == NULL)
-		return (NULL);
-	memset(bit, -1, ft_strlen(str) * 8);
-	char *tmp_str;
-	memmove(tmp_str, str, ft_strlen(str));
-	while (tmp_str[i] != '\0')
+	while (str[i] != '\0')
 	{
-		while ((k + 1) % 8 != 0)
+		k = 0;
+		bit_up = 7;
+		while (k < 8)
 		{
-			if ((tmp_str[i] & 1) == 1)
-				bit[k] = 1;
-			else if ((tmp_str[i] & 1) == 0)
-				bit[k] = 0;
+			if ((str[i] & 1 << bit_up) == 1)
+				kill(server_pid, SIGUSR1);
+			else if ((str[i] & 1 << bit_up) == 0)
+				kill(server_pid, SIGUSR2);
+			usleep(50);
 			k++;
-			tmp_str[i] = tmp_str[i] >> 1;
+			bit_up--;
 		}
-		i++;
 	}
-	return (bit);
-}
-
-int send_bit(pid_t server_pid, int *bit)
-{
-	int	i;
-
-	if (bit == NULL)
-		return (0);
-	i = 0;
-	while (bit[i] != -1)
-	{
-		if (bit[i] == 1)
-			kill(server_pid, SIGUSR1);
-		else if (bit[i] == 0)
-			kill(server_pid, SIGUSR2);
-		else
-			return (-1);
-		usleep(50);
-		i++;
-	}
-	return (i);
+	return (str);
 }
