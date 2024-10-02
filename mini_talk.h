76f1dbfe100fee6e6fd7ecd8eb7f3801b60b5fdc
bigCoDult <qqqqmmss11@gmail.com>
2024-10-02 13:47:46 +0900

    norm : mini_talk.h

--- mini_talk.h
+++ mini_talk.h
@@ -1,21 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mini_talk.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanbaek <sanbaek@student.42gyeongsan.kr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/02 13:46:47 by sanbaek           #+#    #+#             */
+/*   Updated: 2024/10/02 13:47:25 by sanbaek          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINI_TALK_H
 # define MINI_TALK_H
 
-// # include "libft.h"
-# include "./libft/libft.h"
+# include "libft.h"
 # include <unistd.h>
 # include <signal.h>
 # include <sched.h>
 
 typedef struct s_byte
 {
-	unsigned char	byte;
-	int				received_bit;
+	char	byte;
+	int		received_bit;
 }	t_byte;
 
 int		validate_str(char *str);
 char	*send_string(pid_t server_pid, char *str);
 void	combine_bit(int sig);
-
 
 #endif /* MINI_TALK_H */
