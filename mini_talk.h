4378a11601812702b6217b88b3b8f0065239af28
bigCoDult <qqqqmmss11@gmail.com>
2024-10-03 05:28:20 +0900

    add : header, validate_pid(), cmp_itoa_atoi()

--- mini_talk.h
+++ mini_talk.h
@@ -6,7 +6,7 @@
 /*   By: sanbaek <sanbaek@student.42gyeongsan.kr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 13:46:47 by sanbaek           #+#    #+#             */
-/*   Updated: 2024/10/02 13:47:25 by sanbaek          ###   ########.fr       */
+/*   Updated: 2024/10/03 05:03:51 by sanbaek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,8 @@ typedef struct s_byte
 	int		received_bit;
 }	t_byte;
 
-int		validate_str(char *str);
+int		validate_pid(char *str);
+int		cmp_itoa_atoi(char *str);
 char	*send_string(pid_t server_pid, char *str);
 void	combine_bit(int sig);
 
