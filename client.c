c93f5344c1f7bf138e843f3ac283c80dcaa4d09f
bigCoDult <qqqqmmss11@gmail.com>
2024-10-03 05:27:40 +0900

    usleep(800)

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: sanbaek <sanbaek@student.42gyeongsan.kr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 13:25:36 by sanbaek           #+#    #+#             */
-/*   Updated: 2024/10/03 05:11:44 by sanbaek          ###   ########.fr       */
+/*   Updated: 2024/10/03 05:22:53 by sanbaek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ char	*send_string(pid_t server_pid, char *str)
 				kill(server_pid, SIGUSR1);
 			else
 				kill(server_pid, SIGUSR2);
-			usleep(400);
+			usleep(800);
 			bit_up--;
 		}
 		i++;
